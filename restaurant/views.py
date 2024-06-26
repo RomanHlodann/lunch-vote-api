b1879d2bd930818000fe30ccf4fee7dc4713afdc
RomanHlodann <roman.hlodann@gmail.com>
2024-06-26 13:55:31 +0300

    add viewsets for Menu and Restaurant

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.db.models import Prefetch
+from rest_framework import viewsets
+
+from restaurant.models import Restaurant, Menu
+from restaurant.serializers import (
+    RestaurantSerializer,
+    RestaurantDetailSerializer,
+    MenuSerializer,
+    MenuListSerializer,
+    MenuDetailSerializer
+)
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+
+    def get_queryset(self):
+        queryset = Restaurant.objects.all()
+        if self.action == "retrieve":
+            today = datetime.date.today()
+            queryset = (queryset.prefetch_related(
+                Prefetch("menus", queryset=Menu.objects.filter(date=today))
+            ))
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return RestaurantDetailSerializer
+        return RestaurantSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.select_related("restaurant")
+    serializer_class = MenuSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MenuListSerializer
+        elif self.action == "retrieve":
+            return MenuDetailSerializer
+        return self.serializer_class
