23a688df2b8edb61ed521947dc8891ca18cc9297
RomanHlodann <roman.hlodann@gmail.com>
2024-06-26 13:56:21 +0300

    add restaurants urls to project urls

--- lunch/urls.py
+++ lunch/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("user.urls", namespace="user")),
+    path("api/restaurants/", include("restaurant.urls", namespace="restaurant")),
 ]
