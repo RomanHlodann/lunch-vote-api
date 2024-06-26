ded5ccaada29bb0c64c4b2ded9182c35562372ef
RomanHlodann <roman.hlodann@gmail.com>
2024-06-26 14:47:01 +0300

    add get_queryset with filtering query params in MenuViewSet

--- restaurant/views.py
+++ restaurant/views.py
@@ -33,6 +33,30 @@ class MenuViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     pagination_class = DefaultPagination
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        queryset = self.queryset
+        lunch_date = self.request.query_params.get("lunch_date")
+        all_dates = self.request.query_params.get("all")
+        restaurants = self.request.query_params.get("restaurants")
+
+        today = datetime.today()
+        if not all_dates or not all_dates.lower() in ("yes", "true", "t", "1"):
+            if lunch_date:
+                today = datetime.strptime(lunch_date, "%Y-%m-%d").date()
+
+            queryset = queryset.filter(date=today)
+
+        if restaurants:
+            restaurant_ids = self._params_to_ints(restaurants)
+            queryset = queryset.filter(restaurant_id__in=restaurant_ids)
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return MenuListSerializer
