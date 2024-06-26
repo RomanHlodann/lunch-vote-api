2013201217a5e5b06493c0585726e48940d1efa5
RomanHlodann <roman.hlodann@gmail.com>
2024-06-26 12:19:23 +0300

    add Restaurant and Menu models & migrations

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+def menu_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.restaurant.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/menus/", filename)
+
+
+class Menu(models.Model):
+    image = models.ImageField(upload_to=menu_image_file_path)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        related_name="menus"
+    )
