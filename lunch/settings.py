ca68ac91f0543a0b04d87742cb610fd0bbd57ac6
RomanHlodann <roman.hlodann@gmail.com>
2024-06-26 10:52:08 +0300

    add DEFAULT_AUTHENTICATION_CLASS JWTAuthentication

--- lunch/settings.py
+++ lunch/settings.py
@@ -122,4 +122,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
 AUTH_USER_MODEL = "user.User"
