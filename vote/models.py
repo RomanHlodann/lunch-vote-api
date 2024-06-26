a24b80e13a651b5be006799bee8702c30c028b1b
RomanHlodann <roman.hlodann@gmail.com>
2024-06-26 15:50:07 +0300

    add Vote model & migrations

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from restaurant.models import Menu
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    menu = models.ForeignKey(
+        Menu,
+        on_delete=models.CASCADE,
+        related_name="votes"
+    )
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "menu", "created_at",)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.menu}"
